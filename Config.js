a2d59d8078c671b7700ca19db9dd37f9d276543f
SStudios20 <78222767+SStudios20@users.noreply.github.com>
2022-07-31 02:02:29 -0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,13 +1,13 @@
 module.exports = {
     SUFFIX: "#",
     ADMIN: "", // Enter your phone number with country code  and without (+)
-    STICKER_AUTHOR: "Ryuk's Bot",
-    STICKER_NAME: "Bot-wa",
+    STICKER_AUTHOR: "SStudios",
+    STICKER_NAME: "KomiSticker",
     LANGUAGUE: 'en', // for text to audio // supported language list -> https://www.npmjs.com/package/gtts
     SYMBOL: '*⚝*', // for 𓃡 !help
     ARROW_SYMBOL: '*↠*', // for 𓃡 !help ↠ to see how to use
     MONGO_DB_URI: "",// get mongodb uri(create a cluster to get mongo db uri) from here https://www.mongodb.com/ or visit here https://www.youtube.com/watch?v=YZ66YqGPip8
-    BOT_NUMBER: "918521456930", // enter your bot number here with country code and without (+)
+    BOT_NUMBER: "13052509633", // enter your bot number here with country code and without (+)
     CONTACT_ME: "https://t.me/ryuk_me", // enter you social link here so that people can contact you regarding bot access
     MAX_MUTE_TIME: 300, // where 300 is in minutes
     COMMAND_TIMEOUT: 5 // where 10 is in seconds
